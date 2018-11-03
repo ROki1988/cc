d3530dbae1253f7293d9fb2a9deaef137bd030e7
ROki1988 <roki.swindler@gmail.com>
2018-11-03 09:04:34 +0900

    impl add and sub

--- 9cc.c
+++ 9cc.c
@@ -9,12 +9,29 @@ int main(int argc, char const *argv[])
         return 1;
     }
 
+    char *p = argv[1];
+
     printf(".intel_syntax noprefix\n");
     printf(".global main\n");
     printf("main:\n");
-    printf("  mov rax, %d\n", atoi(argv[1]));
-    printf("  ret\n");
+    printf("  mov rax, %ld\n", strtol(p, &p, 10));
 
-   
+    
+    while(*p){
+        
+        if (*p == '+') {
+            p++;
+            printf("  add rax, %ld\n", strtol(p, &p, 10));
+        }
+        else if(*p == '-') {
+            p++;
+            printf("  sub rax, %ld\n", strtol(p, &p, 10));
+        }
+        else {
+            fprintf(stderr, "予期せぬ文字です: '%c'\n", *p);
+        }
+    }
+        
+    printf("  ret\n");
     return 0;
 }
